a8ebe009d1d1858d7973526d4553e3f013ecf0de
mclark-99 <MClark616@wcupa.edu>
2026-03-12 15:56:08 -0400

    Add basic HTTP server in app.py

---
+++ component1/app.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    self.wfile.write(b"Hello from component1 (Python backend)!")
+
+server = HTTPServer(("0.0.0.0, 8000), Handler)
+print("component1 running on port 8000)
+server.serve_forever()
