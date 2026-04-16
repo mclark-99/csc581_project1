c18107cbd78353d1e3b9b597345473add4f63906
Maiti Clark <mclark616@wcupa.edu>
2026-04-16 19:05:32 -0400

    Add education section with styling and layout

--- component1/app.py
+++ component1/app.py
@@ -7,11 +7,47 @@ def get_data():
     return jsonify({
         "name": "Maiti Clark",
         "bio": "Master’s student in Computer Science at West Chester University.",
-        "experience": [
-            "Lead Bartender - La Scala’s Fire",
-            "Web Specialist - Unifeyed LLC",
-            "Irish Dance Performer & Instructor"
+        
+        "education": [
+    {
+        "school": "West Chester University",
+        "location": "West Chester, PA",
+        "degree": "M.S. - Masters of Computer Science",
+        "dates": "January 2025 – Current",
+        "details": [
+            "Courses: Programming Languages, Advanced Seminar: Computer Security, Foundations of Computer Science, Networks and Data Communications, Advanced Seminar: Data Visualization",
+            "Upsilon Pi Epsilon (UPE) Honor Society Member – West Chester University Chapter"
         ]
+    },
+    {
+        "school": "University College Dublin",
+        "location": "Dublin, Ireland",
+        "degree": "B.A. - Double Major in Information & Social Computing with Sociology",
+        "dates": "September 2018 – May 2021",
+        "details": [
+            "Courses: Artificial Intelligence, The Digital Self, Information Ethics, Digital Marketing, Web Publishing, Information Architecture",
+            "Computer-Mediated Communication, Social Media & Participation in an Online World"
+        ]
+    }
+]
+
+        "experience": [
+    {
+        "title": "Lead Bartender",
+        "company": "La Scala’s Fire",
+        "description": "Managed staff, created cocktail menus, and handled high-volume service."
+    },
+    {
+        "title": "Web Specialist",
+        "company": "Unifeyed LLC",
+        "description": "Developed and maintained websites, improved SEO performance."
+    },
+    {
+        "title": "Irish Dance Performer & Instructor",
+        "company": "",
+        "description": "Performed professionally and choreographed routines."
+    }
+]
     })
 
 app.run(host="0.0.0.0", port=8000)
--- component2/script.js
+++ component2/script.js
@@ -8,8 +8,39 @@ fetch('/api')
 
       data.experience.forEach(job => {
         const li = document.createElement("li");
-        li.inerText = job;
+
+        li.innerHTML = `
+          <div class="job">
+            <div class="job-title">${job.title} ${job.company ? "– " + job.company : ""}</div>
+            <div class="job-desc">${job.description}</div>
+          </div>
+          `;
+
         expList.appendChild(li);
+      });
+
+      const eduList = document.getElementById("education");
+
+      data.education.forEach(edu => {
+        const li = document.createElement("li");
+
+        let detailsHTML = "";
+        edu.details.forEach(item => {
+          detailsHTML += `<li>${item}</li>`;
+        });
+
+        li.innerHTML = `
+          <div class="edu">
+            <div class="edu-header">
+              <span class="edu-school">${edu.school} | ${edu.location}</span>
+              <span class="edu-dates">${edu.dates}</span>
+            </div>
+            <div class="edu-degree">${edu.degree}</div>
+            <ul class="edu-details">${detailsHTML}</ul>
+          </div>
+        `;
+
+        eduList.appendChild(li);
       });
   })
   .catch(err => console.error(err));
